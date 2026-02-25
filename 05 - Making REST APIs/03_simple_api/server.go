99914a695128f2bb8c7233b7db7e179083d1f77a
yodenharsh <harshmorayya@ymail.com>
2026-02-25 12:37:51 +0530

    add self signed certificate for TLS

--- 05 - Making REST APIs/03_simple_api/server.go
+++ 05 - Making REST APIs/03_simple_api/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 func main() {
@@ -24,6 +27,26 @@ func main() {
 		fmt.Fprintf(w, "Handling incoming users")
 	})
 
+	cert := "cert.pem"
+	key := "key.pem"
+
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	server := &http.Server{
+		Addr:      fmt.Sprintf(":%d", port),
+		TLSConfig: tlsConfig,
+		Handler:   nil,
+	}
+
+	http2.ConfigureServer(server, &http2.Server{})
+
 	fmt.Println("Server is running on port", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	server.ListenAndServeTLS(cert, key)
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// if err != nil {
+	// 	log.Fatalln("Error starting server:", err)
+	// }
 }
