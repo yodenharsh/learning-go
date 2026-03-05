b365bd4543ee3fbc2da0f9e53695ea969c2db5d4
yodenharsh <harshmorayya@ymail.com>
2026-03-05 12:28:01 +0530

    Try unmarshaling JSON in request body

--- 05 - Making REST APIs/04_main_api_project/cmd/api/server.go
+++ 05 - Making REST APIs/04_main_api_project/cmd/api/server.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	City string `json:"city"`
+}
 
 func main() {
 
@@ -22,6 +30,44 @@ func main() {
 		case http.MethodGet:
 			w.Write([]byte("Hello GET Method on Teachers Route"))
 		case http.MethodPost:
+			// Parsing form here
+			err := r.ParseForm()
+			if err != nil {
+				http.Error(w, "Error parsing form", http.StatusBadRequest)
+				log.Println("Error parsing form:", err)
+				return
+			}
+
+			fmt.Println("Form data received:", r.Form)
+
+			// Prepare response data
+			response := make(map[string]string)
+			for key, values := range r.Form {
+				if len(values) > 0 {
+					response[key] = values[0]
+				}
+			}
+			fmt.Println("Response data prepared:", response)
+
+			// Parsing raw json
+			jsonData, err := io.ReadAll(r.Body)
+			if err != nil {
+				return
+			}
+			defer r.Body.Close()
+
+			fmt.Println("Raw body: ", string(jsonData))
+
+			var userInstance User
+			err = json.Unmarshal(jsonData, &userInstance)
+			if err != nil {
+				http.Error(w, "Error parsing JSON", http.StatusBadRequest)
+				log.Println("Error parsing JSON:", err)
+				return
+			}
+
+			fmt.Println("Parsed JSON data:", userInstance)
+
 			w.Write([]byte("Hello POST Method on Teachers Route"))
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
