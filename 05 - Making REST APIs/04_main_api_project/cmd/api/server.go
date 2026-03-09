5a8fd622e69cdd148956afdadb9d1a32552a81b9
yodenharsh <harshmorayya@ymail.com>
2026-03-09 10:18:32 +0530

    convert http handlers to mux

--- 05 - Making REST APIs/04_main_api_project/cmd/api/server.go
+++ 05 - Making REST APIs/04_main_api_project/cmd/api/server.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strings"
 )
 
 type User struct {
@@ -21,55 +18,11 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 func teachersHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("Received request for /teachers route on ", r.Method, " method")
 	switch r.Method {
 	case http.MethodGet:
 
-		fmt.Println(r.URL.Path)
-		path := strings.TrimPrefix(r.URL.Path, "/teachers/")
-		userId := strings.TrimSuffix(path, "/")
-		fmt.Println("Extracted userId: ", userId)
-
 		w.Write([]byte("Hello GET Method on Teachers Route"))
 	case http.MethodPost:
-		// Parsing form here
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "Error parsing form", http.StatusBadRequest)
-			log.Println("Error parsing form:", err)
-			return
-		}
-
-		fmt.Println("Form data received:", r.Form)
-
-		// Prepare response data
-		response := make(map[string]string)
-		for key, values := range r.Form {
-			if len(values) > 0 {
-				response[key] = values[0]
-			}
-		}
-		fmt.Println("Response data prepared:", response)
-
-		// Parsing raw json
-		jsonData, err := io.ReadAll(r.Body)
-		if err != nil {
-			return
-		}
-		defer r.Body.Close()
-
-		fmt.Println("Raw body: ", string(jsonData))
-
-		var userInstance User
-		err = json.Unmarshal(jsonData, &userInstance)
-		if err != nil {
-			http.Error(w, "Error parsing JSON", http.StatusBadRequest)
-			log.Println("Error parsing JSON:", err)
-			return
-		}
-
-		fmt.Println("Parsed JSON data:", userInstance)
-
 		w.Write([]byte("Hello POST Method on Teachers Route"))
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -78,10 +31,6 @@ func teachersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func studentsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Query params: ", r.URL.Query())
-	sortBy := r.URL.Query().Get("sortby")
-	fmt.Println("Sort by: ", sortBy)
-
 	w.Write([]byte("Placeholder for students route"))
 }
 
@@ -94,15 +43,17 @@ func main() {
 	port := "3000"
 	fmt.Println("Server is running on port ", port)
 
-	http.HandleFunc("/", rootHandler)
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/teachers/", teachersHandler)
+	mux.HandleFunc("/", rootHandler)
 
-	http.HandleFunc("/students/", studentsHandler)
+	mux.HandleFunc("/teachers/", teachersHandler)
 
-	http.HandleFunc("/execs/", execsHandler)
+	mux.HandleFunc("/students/", studentsHandler)
 
-	err := http.ListenAndServe(":"+port, nil)
+	mux.HandleFunc("/execs/", execsHandler)
+
+	err := http.ListenAndServe(":"+port, mux)
 	if err != nil {
 		log.Fatal("Error starting server:", err)
 	}
