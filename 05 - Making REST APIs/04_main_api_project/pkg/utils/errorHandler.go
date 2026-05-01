8cc4d1f2519b36eb164c2cdb58b2e4c913080ed1
yodenharsh <harshmorayya@ymail.com>
2026-05-01 23:09:50 +0530

    fix warning in ErrorF

--- 05 - Making REST APIs/04_main_api_project/pkg/utils/errorHandler.go
+++ 05 - Making REST APIs/04_main_api_project/pkg/utils/errorHandler.go
@@ -10,5 +10,5 @@ func ErrorHandler(err error, message string) error {
 	errorLogger := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 	errorLogger.Println(message, err)
 
-	return fmt.Errorf(message)
+	return fmt.Errorf("%s", message)
 }
