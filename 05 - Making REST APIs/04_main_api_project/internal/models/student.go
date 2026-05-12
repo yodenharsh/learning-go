2fe3bee69d56fc2ac7920037d3d2fb2f52d257ef
yodenharsh <harshmorayya@ymail.com>
2026-05-12 12:01:37 +0530

    add dbtag for Student model

--- 05 - Making REST APIs/04_main_api_project/internal/models/student.go
+++ 05 - Making REST APIs/04_main_api_project/internal/models/student.go
@@ -1,9 +1,9 @@
 package models
 
 type Student struct {
-	Id        int    `json:"id,omitempty"`
-	FirstName string `json:"firstName,omitempty"`
-	LastName  string `json:"lastName,omitempty"`
-	Email     string `json:"email,omitempty"`
-	Class     string `json:"class,omitempty"`
+	Id        int    `json:"id,omitempty" db:"id,omitempty"`
+	FirstName string `json:"firstName,omitempty" db:"first_name,omitempty"`
+	LastName  string `json:"lastName,omitempty" db:"last_name,omitempty"`
+	Email     string `json:"email,omitempty" db:"email,omitempty"`
+	Class     string `json:"class,omitempty" db:"class,omitempty"`
 }
