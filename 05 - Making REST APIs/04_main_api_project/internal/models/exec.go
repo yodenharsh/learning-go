f2c855fd39b0cc75c3c971ff4b9d6db8038b2e39
yodenharsh <harshmorayya@ymail.com>
2026-05-14 12:48:31 +0530

    add the execs model

--- 05 - Making REST APIs/04_main_api_project/internal/models/exec.go
+++ 05 - Making REST APIs/04_main_api_project/internal/models/exec.go
@@ -1,6 +1,18 @@
 package models
 
+import "database/sql"
+
 type Exec struct {
-	Id int `json:"id,omitempty"`
-	// This is a to-do
+	Id                    int            `json:"id,omitempty"`
+	FirstName             string         `json:"firstName,omitempty"`
+	LastName              string         `json:"lastName,omitempty"`
+	Email                 string         `json:"email,omitempty"`
+	Username              string         `json:"username,omitempty"`
+	Password              string         `json:"password,omitempty"`
+	InactiveStatus        bool           `json:"inactiveStatus,omitempty"`
+	Role                  string         `json:"role,omitempty"`
+	PasswordResetCode     sql.NullString `json:"passwordResetCode"`
+	PasswordCodeExpiresAt sql.NullString `json:"passwordCodeExpiresAt"`
+	PasswordChangedAt     sql.NullString `json:"passwordChangedAt"`
+	UserCreatedAt         sql.NullString `json:"userCreatedAt"`
 }
