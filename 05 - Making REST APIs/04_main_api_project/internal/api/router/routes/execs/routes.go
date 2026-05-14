6b3493acf48e98157f6c0494b70ac477b544315c
yodenharsh <harshmorayya@ymail.com>
2026-05-14 12:30:54 +0530

    (partial) add routes for exec handlers

--- 05 - Making REST APIs/04_main_api_project/internal/api/router/routes/execs/routes.go
+++ 05 - Making REST APIs/04_main_api_project/internal/api/router/routes/execs/routes.go
@@ -7,5 +7,16 @@ import (
 
 func Register(mux *http.ServeMux) {
 	mux.HandleFunc("GET /execs", handlers.ExecsHandler)
+	mux.HandleFunc("GET /execs/{id}", handler.)
 	mux.HandleFunc("POST /execs", handlers.PostExecsHandler)
+	mux.HandleFunc("PATCH /execs", handlers.)
+	mux.HandleFunc("PATCH /execs/{id}", handlers.)
+	mux.HandleFunc("DELETE /execs/{id}", handlers.)
+
+	mux.HandleFunc("POST /execs/login", handlers.)
+	mux.HandleFunc("POST /execs/logout", handlers.)
+	mux.HandleFunc("POST /execs/forgotpassword", handlers.)
+	mux.HandleFunc("POST /execs/password/reset/{resetCode}", handlers.)
+	mux.HandleFunc("POST /execs/{id}/updatepassword", handlers.)
 }
+``
