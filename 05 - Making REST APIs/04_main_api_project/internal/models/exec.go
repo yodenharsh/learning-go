a75a2331dbc2b541c193553ac85d7a884a3433bc
yodenharsh <harshmorayya@ymail.com>
2026-05-14 20:44:09 +0530

    update exec model with correct tags

--- 05 - Making REST APIs/04_main_api_project/internal/models/exec.go
+++ 05 - Making REST APIs/04_main_api_project/internal/models/exec.go
@@ -3,16 +3,16 @@ package models
 import "database/sql"
 
 type Exec struct {
-	Id                    int            `json:"id,omitempty"`
-	FirstName             string         `json:"firstName,omitempty"`
-	LastName              string         `json:"lastName,omitempty"`
-	Email                 string         `json:"email,omitempty"`
-	Username              string         `json:"username,omitempty"`
-	Password              string         `json:"password,omitempty"`
-	InactiveStatus        bool           `json:"inactiveStatus,omitempty"`
-	Role                  string         `json:"role,omitempty"`
-	PasswordResetCode     sql.NullString `json:"passwordResetCode"`
-	PasswordCodeExpiresAt sql.NullString `json:"passwordCodeExpiresAt"`
-	PasswordChangedAt     sql.NullString `json:"passwordChangedAt"`
-	UserCreatedAt         sql.NullString `json:"userCreatedAt"`
+	Id                    int            `json:"id,omitempty" db:"id"`
+	FirstName             string         `json:"firstName,omitempty" db:"first_name"`
+	LastName              string         `json:"lastName,omitempty" db:"last_name"`
+	Email                 string         `json:"email,omitempty" db:"email"`
+	Username              string         `json:"username,omitempty" db:"username"`
+	Password              string         `json:"password,omitempty" db:"password"`
+	InactiveStatus        bool           `json:"inactiveStatus,omitempty" db:"inactive_status"`
+	Role                  string         `json:"role,omitempty" db:"role"`
+	PasswordResetCode     sql.NullString `json:"passwordResetCode" db:"password_reset_code"`
+	PasswordCodeExpiresAt sql.NullString `json:"passwordCodeExpiresAt" db:"password_code_expires_at"`
+	PasswordChangedAt     sql.NullString `json:"passwordChangedAt" db:"password_changed_at"`
+	UserCreatedAt         sql.NullString `json:"userCreatedAt" db:"user_created_at"`
 }
