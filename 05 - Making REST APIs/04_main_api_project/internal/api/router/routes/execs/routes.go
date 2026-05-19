65a810c79303aa3af5786f1433f8e3ed84a86fb9
yodenharsh <harshmorayya@ymail.com>
2026-05-19 12:07:33 +0530

    add jwt middleware to some routes as middleware

--- 05 - Making REST APIs/04_main_api_project/internal/api/router/routes/execs/routes.go
+++ 05 - Making REST APIs/04_main_api_project/internal/api/router/routes/execs/routes.go
@@ -3,19 +3,20 @@ package execs
 import (
 	"net/http"
 	"restapi/internal/api/handlers"
+	mw "restapi/internal/api/middlewares"
 )
 
 func Register(mux *http.ServeMux) {
-	mux.HandleFunc("GET /execs", handlers.GetExecsHandler)
-	mux.HandleFunc("GET /execs/{id}", handlers.GetExecByIdHandler)
-	mux.HandleFunc("POST /execs", handlers.PostExecsHandler)
-	mux.HandleFunc("PATCH /execs", handlers.PatchExecsHandler)
-	mux.HandleFunc("PATCH /execs/{id}", handlers.PatchExecByIdHandler)
-	mux.HandleFunc("DELETE /execs/{id}", handlers.DeleteExecByIdHandler)
+	mux.Handle("GET /execs", mw.JwtMiddleware(http.HandlerFunc(handlers.GetExecsHandler)))
+	mux.Handle("GET /execs/{id}", mw.JwtMiddleware(http.HandlerFunc(handlers.GetExecByIdHandler)))
+	mux.Handle("POST /execs", mw.JwtMiddleware(http.HandlerFunc(handlers.PostExecsHandler)))
+	mux.Handle("PATCH /execs", mw.JwtMiddleware(http.HandlerFunc(handlers.PatchExecsHandler)))
+	mux.Handle("PATCH /execs/{id}", mw.JwtMiddleware(http.HandlerFunc(handlers.PatchExecByIdHandler)))
+	mux.Handle("DELETE /execs/{id}", mw.JwtMiddleware(http.HandlerFunc(handlers.DeleteExecByIdHandler)))
 
-	// GetExecsHandler is temporary. Actual handlers need to be implemented for these routes
 	mux.HandleFunc("POST /execs/login", handlers.LoginHandler)
 	mux.HandleFunc("POST /execs/logout", handlers.LogoutHandler)
+	// GetExecsHandler is temporary. Actual handlers need to be implemented for these routes
 	mux.HandleFunc("POST /execs/forgotpassword", handlers.GetExecsHandler)
 	mux.HandleFunc("POST /execs/password/reset/{resetCode}", handlers.GetExecsHandler)
 	mux.HandleFunc("POST /execs/{id}/updatepassword", handlers.GetExecsHandler)
