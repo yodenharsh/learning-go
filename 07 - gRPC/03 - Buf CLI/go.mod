8a9c468c63d4e2d1c28b3c5399ce5671750d1bdd
yodenharsh <harshmorayya@ymail.com>
2026-06-11 13:36:24 +0530

    use buf cli for the first time

---
+++ 07 - gRPC/03 - Buf CLI/go.mod
@@ -0,0 +1,11 @@
+module bufexample
+
+go 1.26.4
+
+require (
+	connectrpc.com/connect v1.20.0
+	golang.org/x/net v0.56.0
+	google.golang.org/protobuf v1.36.11
+)
+
+require golang.org/x/text v0.38.0 // indirect
---
+++ 07 - gRPC/03 - Buf CLI/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	petv1 "bufexample/gen/pet/v1"
+
+	"bufexample/gen/pet/v1/petv1connect"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+const address = "localhost:8080"
+
+// petStoreServiceServer implements the PetStoreService API.
+type petStoreServiceServer struct {
+	petv1connect.UnimplementedPetStoreServiceHandler
+}
+
+// PutPet adds the pet associated with the given request into the PetStore.
+func (s *petStoreServiceServer) PutPet(
+	_ context.Context,
+	req *petv1.PutPetRequest,
+) (*petv1.PutPetResponse, error) {
+	pet := &petv1.Pet{Name: req.GetName(), PetType: req.PetType}
+	log.Printf("PutPet received a %v named %s", pet.GetPetType(), pet.GetName())
+	return &petv1.PutPetResponse{Pet: pet}, nil
+}
+
+func main() {
+	mux := http.NewServeMux()
+	path, handler := petv1connect.NewPetStoreServiceHandler(&petStoreServiceServer{})
+	mux.Handle(path, handler)
+	fmt.Println("... Listening on", address)
+	http.ListenAndServe(
+		address,
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
